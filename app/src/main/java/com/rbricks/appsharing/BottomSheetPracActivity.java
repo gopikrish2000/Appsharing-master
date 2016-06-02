f33f37b3d04322a812dcbe2595ef0aa6bb6614f5
gopikrishna <gopikrish2000@gmail.com>
2016-06-02 15:21:07 +0530

    Added Bottom sheet behaviour

---
+++ app/src/main/java/com/rbricks/appsharing/BottomSheetPracActivity.java
@@ -0,0 +1,53 @@
+package com.rbricks.appsharing;
+
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BottomSheetPracActivity extends ActionBarActivity implements View.OnClickListener {
+
+    BottomSheetBehavior<View> mBottomSheetBehavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet_prac);
+        View bottomSheet = findViewById( R.id.bottom_sheet );
+        Button button1 = (Button) findViewById( R.id.button_1 );
+        Button button2 = (Button) findViewById( R.id.button_2 );
+        Button button3 = (Button) findViewById( R.id.button_3 );
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        mBottomSheetBehavior.setPeekHeight(300);
+//        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    mBottomSheetBehavior.setPeekHeight(0);
+                }
+            }
+
+            @Override
+            public void onSlide(View bottomSheet, float slideOffset) {
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch( v.getId() ) {
+            case R.id.button_1: {
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                break;
+            }
+        }
+    }
+}
