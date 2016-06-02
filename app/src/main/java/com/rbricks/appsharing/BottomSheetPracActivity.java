6053eeddbd8c760fc5bc3fa7e1331b01761ec56c
gopikrishna <gopikrish2000@gmail.com>
2016-06-02 15:59:52 +0530

    Added Bottom sheet behaviour with Swipe Refresh Layout , not working completely

--- app/src/main/java/com/rbricks/appsharing/BottomSheetPracActivity.java
+++ app/src/main/java/com/rbricks/appsharing/BottomSheetPracActivity.java
@@ -1,14 +1,17 @@
 package com.rbricks.appsharing;
 
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class BottomSheetPracActivity extends ActionBarActivity implements View.OnClickListener {
 
     BottomSheetBehavior<View> mBottomSheetBehavior;
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,17 @@ public class BottomSheetPracActivity extends ActionBarActivity implements View.O
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(BottomSheetPracActivity.this, "Layout Refreshing", Toast.LENGTH_LONG).show();
+//                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+
 
         mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
         mBottomSheetBehavior.setPeekHeight(300);
@@ -30,13 +44,26 @@ public class BottomSheetPracActivity extends ActionBarActivity implements View.O
         mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
             public void onStateChanged(View bottomSheet, int newState) {
+                System.out.println("state which is getting changed is = " + newState);
                 if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
-                    mBottomSheetBehavior.setPeekHeight(0);
+//                    Toast.makeText(BottomSheetPracActivity.this, "Write Refresh Logic", Toast.LENGTH_LONG);
+//                    System.out.println(" Do Refresh Code " );
+//                    mBottomSheetBehavior.setPeekHeight(0);
                 }
             }
 
             @Override
             public void onSlide(View bottomSheet, float slideOffset) {
+                if (slideOffset == 0) {
+//                    Toast.makeText(BottomSheetPracActivity.this, "Write Refresh Logic", Toast.LENGTH_LONG);
+                    System.out.println(" Do Refresh Code in Behaviour" );
+                    mSwipeRefreshLayout.setRefreshing(true);
+
+//                    mBottomSheetBehavior.setPeekHeight(0);
+
+                }
+                System.out.println("Slide got called = " + slideOffset);
+
             }
         });
     }
