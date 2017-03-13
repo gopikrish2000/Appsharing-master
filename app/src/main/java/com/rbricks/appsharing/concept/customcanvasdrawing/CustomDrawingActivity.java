a64b1700896653a8cbbf1524d5d73007fb090608
gopi <gopi@dunzo.in>
2017-03-13 18:14:01 +0530

    Custom Dotted line drawing between views in oncreate

--- app/src/main/java/com/rbricks/appsharing/concept/customcanvasdrawing/CustomDrawingActivity.java
+++ app/src/main/java/com/rbricks/appsharing/concept/customcanvasdrawing/CustomDrawingActivity.java
@@ -1,17 +1,12 @@
 package com.rbricks.appsharing.concept.customcanvasdrawing;
 
-import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.rbricks.appsharing.R;
-import com.rbricks.appsharing.utils.CommonUtils;
-
-import static com.rbricks.appsharing.utils.CommonUtils.nonNull;
 
 public class CustomDrawingActivity extends AppCompatActivity {
 
@@ -33,11 +28,6 @@ public class CustomDrawingActivity extends AppCompatActivity {
         secondIv = (ImageView) findViewById(R.id.second_iv);
         parentView = ((RelativeLayout) findViewById(R.id.parent_layout));
         firstIv.postDelayed(this::drawLine, 50);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
     }
 
     private void drawLine() {
