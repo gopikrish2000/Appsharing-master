58d96a76d8ec05d8ba6195b0b67cdcd5a2627605
gopi <gopi@hike.in>
2017-08-18 20:11:21 +0530

    Android Camera Forward Video taken

--- app/src/main/java/com/rbricks/appsharing/newconcept/hikecameraconcepts/basiccamera/AndroidCameraForwardActivity.java
+++ app/src/main/java/com/rbricks/appsharing/newconcept/hikecameraconcepts/basiccamera/AndroidCameraForwardActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.VideoView;
 
 import com.rbricks.appsharing.R;
 
@@ -15,12 +18,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static com.rbricks.appsharing.concept.notesapp.utils.ViewUtils.setGone;
+
 
 public class AndroidCameraForwardActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int VIDEO_REQ_CODE = 15;
     Bitmap bitmap;
     private ImageView camForwIv;
     private int CAMERA_REQ_CODE = 4;
     private int GALLERY_REQ_CODE = 10;
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +35,16 @@ public class AndroidCameraForwardActivity extends AppCompatActivity implements V
         setContentView(R.layout.activity_android_camera_forward);
 
         camForwIv = (ImageView) findViewById(R.id.cam_forw_iv);
+        mVideoView = ((VideoView) findViewById(R.id.cam_for_videoview));
         findViewById(R.id.cam_forw_takepic).setOnClickListener(this);
         findViewById(R.id.cam_forw_gallerybtn).setOnClickListener(this);
+        findViewById(R.id.cam_forw_takevideo).setOnClickListener(this);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         InputStream stream = null;
-        if ((requestCode == GALLERY_REQ_CODE || requestCode == CAMERA_REQ_CODE) && resultCode == Activity.RESULT_OK)
+        if ((requestCode == GALLERY_REQ_CODE || requestCode == CAMERA_REQ_CODE) && resultCode == Activity.RESULT_OK) {
             try {
                 // recyle unused bitmaps
                 /*if (bitmap != null) {
@@ -61,6 +70,12 @@ public class AndroidCameraForwardActivity extends AppCompatActivity implements V
                         e.printStackTrace();
                     }
             }
+        } else if (requestCode == VIDEO_REQ_CODE && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            mVideoView.setVideoURI(videoUri);
+//            setGone(mVideoView);
+        }
+
     }
 
     @Override
@@ -72,6 +87,13 @@ public class AndroidCameraForwardActivity extends AppCompatActivity implements V
                 break;
             case R.id.cam_forw_gallerybtn:
                 forwardToPhotoGalleryAndroid();
+                break;
+            case R.id.cam_forw_takevideo:
+                mVideoView.setVisibility(View.VISIBLE);
+                Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takeVideoIntent, VIDEO_REQ_CODE);
+                }
                 break;
         }
     }
