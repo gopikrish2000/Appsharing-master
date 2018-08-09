eace63ef7bc76bb306a499fc800bb5842d590e55
gopi <gopi@hike.in>
2018-08-09 21:15:14 +0530

    constraint layout changes

--- app/src/main/java/com/rbricks/appsharing/concept/constraintlayout/ConstraintLayoutAdvancedActivity.java
+++ app/src/main/java/com/rbricks/appsharing/concept/constraintlayout/ConstraintLayoutAdvancedActivity.java
@@ -2,8 +2,13 @@ package com.rbricks.appsharing.concept.constraintlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.rbricks.appsharing.R;
+
+import java.security.acl.Group;
+
 
 public class ConstraintLayoutAdvancedActivity extends AppCompatActivity {
 
@@ -11,5 +16,13 @@ public class ConstraintLayoutAdvancedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraintlayout_advanced);
+
+        final android.support.constraint.Group firstGroup = findViewById(R.id.first_group_id);
+        firstGroup.setOnClickListener(view -> {
+            Toast.makeText(BasicActivity.this, "clicked ", Toast.LENGTH_LONG).show();
+        });
+        firstGroup.setVisibility(View.GONE);
+        final int[] referencedIds = firstGroup.getReferencedIds();
+        System.out.println("referencedIds = " + referencedIds);
     }
 }
